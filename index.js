24ae696e2b0db3bc00b3d5d9c2eefd893e40f56b
Gabriel <gabriel@garox.org>
2018-09-16 16:35:52 -0300

    basic stuff

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+const fs = require('fs-extra')
 const XlsxPopulate = require('xlsx-populate')
 const get = require('lodash.get')
+const fetch = require('node-fetch')
+
+const URL = 'https://tlobzrztlyxicim.form.io/user/login'
 
 main()
 
 async function main () {
+    const json = await fetchJson(URL)
+    const toWrite = parseJson(json)
     const workbook = await XlsxPopulate.fromBlankAsync()
-    workbook.sheet(0).cell("A1").value([
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ])
+    const sheet = workbook.sheet(0).name('Form')
+    sheet.column("A").width(25)
+    sheet.cell("A1").style("bold", true).value(toWrite)
     workbook.toFileAsync("./outputs/test.xlsx")
+}
+
+async function fetchJson(url) {
+    const response = await fetch(url)
+    return response.json()
+}
+
+function parseJson(json) {
+    const { title, components } = json
+    if (!components) throw console.error('No components')
+    const celdas = components.filter(c => c.type != 'button').map( componente => {
+        return [componente.label, '' ]
+    })
+   return [[title]].concat(celdas)
 }
