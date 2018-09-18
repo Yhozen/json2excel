f0d10fcbb62b71c1fe4aae10904c89bc90f597c7
Gabriel <gabriel@garox.org>
2018-09-18 13:31:19 -0300

    add select

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ function handleComponentType (accumulator, component, sheet){
         case 'password':
         case 'textfield':
             return wTextComp(accumulator, component, sheet)
+        case 'select':
         case 'radio':
             return wRadioComp(accumulator, component, sheet)
         default:
@@ -54,11 +55,11 @@ function wTextComp ({ row, column }, component, sheet) {
     return { row: _maxRowNumber+2, column:1 }
 }
 function wRadioComp ({ row, column }, component, sheet) {
-    const values = '"' + component.values.map(({value}) => value).toString() + '"'
     const { _maxRowNumber } = sheet.row(row).cell(column)
-    .value([[ component.label, '']])
-    .style({ fontSize: 14, fill: 'd9d9d9' })
-    sheet.row(row).cell(column+1)
-    .dataValidation(values)
+        .value([[ component.label, '']])
+        .style({ fontSize: 14, fill: 'd9d9d9' })
+    const valuesArray = component.values || component.data.values // to handle both radio and select with one function
+    const values = '"' + valuesArray.map(({value}) => value).toString() + '"'
+    sheet.row(row).cell(column+1).dataValidation(values)
     return { row: _maxRowNumber+2, column:1 }
 }
