a8fabde0107413cdb6fe20d564ccf31deacab068
Gabriel <gabriel@garox.org>
2018-09-18 13:10:50 -0300

    add radio

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function main () {
     const json = await fetchJson(URL)
     parseJson(json, sheet)
     sheet.column("A").width(25)
-    const range = sheet.cell("A1").style({bold: true, fontSize: 16})
+    const range = sheet.cell("A1").style({bold: true, fontSize: 18})
     console.log(range)
     workbook.toFileAsync("./outputs/test.xlsx")
 }
@@ -41,11 +41,27 @@ function handleComponentType (accumulator, component, sheet){
         case 'password':
         case 'textfield':
             return wTextComp(accumulator, component, sheet)
+        case 'radio':
+            return wRadioComp(accumulator, component, sheet)
         default:
+            return accumulator
     }
 }
 
 function wTextComp ({ row, column }, component, sheet) {
-    const range = sheet.row(row).cell(column).value([[ component.label, 'Write here']])
-    return { row: range._maxRowNumber+2, column:1 }
+    const { _maxRowNumber } = sheet.row(row).cell(column)
+    .value([[ component.label, 'Write here']])
+    .style({ fontSize: 14, fill: 'd9d9d9' })
+    console.log('hola desde', component.label)
+    return { row: _maxRowNumber+2, column:1 }
+}
+function wRadioComp ({ row, column }, component, sheet) {
+    const values = '"' + component.values.map(({value}) => value).toString() + '"'
+    console.log(values)
+    const { _maxRowNumber } = sheet.row(row).cell(column)
+    .value([[ component.label, '']])
+    .style({ fontSize: 14, fill: 'd9d9d9' })
+    sheet.row(row).cell(column+1)
+    .dataValidation(values)
+    return { row: _maxRowNumber+2, column:1 }
 }
