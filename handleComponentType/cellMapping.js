006f66f1b9ca2cf5d75aacdf7ae020eb56a4e64e
Gabriel <gabriel@garox.org>
2018-09-19 16:50:36 -0300

    add cellMapping

---
+++ handleComponentType/cellMapping.js
@@ -0,0 +1,15 @@
+const BASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const mod = (x,n) => (x % n + n) % n // fix mod operator in js
+
+module.exports = function cellMapping ({ row, column }) {
+    return columnToLetter(column) + row
+}
+
+function columnToLetter (column) {
+    let remainder = column % BASE.length
+    let quotient = (column / BASE.length) | 0 // transform to int
+    remainder = mod(remainder-1, BASE.length) 
+    if (remainder == BASE.length-1) quotient -= 1
+    if (quotient == 0) return BASE[remainder]
+    return columnToLetter(quotient) + BASE[remainder]
+}
