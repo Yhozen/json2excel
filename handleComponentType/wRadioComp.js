607d3347984e2384d16865c971013083d8848a21
Gabriel <gabriel@garox.org>
2018-10-03 16:21:31 -0300

    radio select validation

--- handleComponentType/wRadioComp.js
+++ handleComponentType/wRadioComp.js
@@ -4,7 +4,13 @@ module.exports = function wRadioComp (context, component) {
         .style({ fontSize: 14, fill: 'd9d9d9' })
     const valuesArray = component.values || component.data.values // to handle both radio and select with one function
     const values = '"' + valuesArray.map(({value}) => value).toString() + '"'
-    cell.relativeCell(0,1).dataValidation(values)
+    cell.relativeCell(0,1).dataValidation({
+        type: 'list',
+        showErrorMessage: true,
+        error: 'Must be a member of the list',
+        errorTitle: 'Incorrect value',
+        formula1: values //Required
+    })
     context._update( cell.relativeCell(0,1) )
     return context
 }
