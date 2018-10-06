2cc619cd5d1d0789ae40210cd062797035554e49
Gabriel <gabriel@garox.org>
2018-10-06 18:11:10 -0300

    export main

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ const fetch = require('node-fetch')
 const handleComponentType = require('./handleComponentType')
 const GlobalContext = require('./handleComponentType/context/GlobalContext')
 
-const URL = 'https://tlobzrztlyxicim.form.io/user/login'
+const TEST_URL = 'https://tlobzrztlyxicim.form.io/user/login'
 
 
-main()
+module.exports = main
 
-async function main () {
+/**
+ * @param {String} url - URL to a form.io json form
+ * @param {Function} writeCallback - Callback to write data
+ */
+async function main (url = TEST_URL, writeCallback = output) {
     const workbook = await XlsxPopulate.fromBlankAsync()
     const sheet = workbook.sheet(0).name('Form')
-    const json = await fetchJson(URL)
+    const json = await fetchJson(url)
     parseJson(json, sheet)
     sheet.column('A').width(25)
     sheet.gridLinesVisible(false) 
-    workbook.toFileAsync('./outputs/test.xlsx')
+    writeCallback(workbook)
 }
 
 async function fetchJson(url) {
@@ -27,9 +31,11 @@ async function fetchJson(url) {
 function parseJson(json, sheet) {
     const { title, components } = json
     if (!components) throw console.error('No components')
-    const globalContext = new GlobalContext(sheet.cell('A1'))
-    const initialCell = globalContext.y.min.value(title).style({bold: true, fontSize: 18})
-    components.reduce(handleComponentType,
-        globalContext
-    )
+    const initialCell = sheet.cell('A1').value(title).style({bold: true, fontSize: 18})
+    const globalContext = new GlobalContext(initialCell)
+    components.reduce(handleComponentType, globalContext)
+}
+
+function output (workbook) {
+    workbook.toFileAsync('./outputs/test.xlsx')
 }
